795d88acc3dc562fd2aaec9e3af12e94bdafdf4f
yusp48 <kremn2018@yandex.ru>
2024-02-11 22:35:02 +0800

    owu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+use xxhash_rust::xxh3::xxh3_64;
+
 const EMOJIS: [&str; 17] = [
     "<3",
     ":3",
@@ -19,7 +23,7 @@ const EMOJIS: [&str; 17] = [
 ];
 
 const PHRASES: [&str; 8] = [
-    "*snuzzews",
+    "*snuzzews*",
     "*nuzzwes youw chest*",
     "*wicks uw neck*",
     "*paws*",
@@ -29,98 +33,156 @@ const PHRASES: [&str; 8] = [
     "*pounces on u*"
 ];
 
+const SIMPLE_REPLACEMENTS: [(&str, &str); 10] = [
+    ("wove", "wuv"),
+    ("nice", "nyaice"),
+    ("what", "wut"),
+    ("you", "u"),
+    ("the", "da"),
+    ("hewwo", "hewo"),
+    ("cat", "neko"),
+    ("kitty", "neko"),
+    ("cute", "kawaii"),
+    ("hi", "hii"),
+];
+
 fn uwuify(input: &str) -> String {
-    let mut result = String::from(input);
+    let input = input.to_lowercase();
+    let input = input.as_str();
+    let input_bytes = input.as_bytes();
 
-    result = result.replace("love", "luv")
-        .replace("nice", "nyaice")
-        .replace("what", "wut")
-        .replace("you", "u")
-        .replace("the", "da");
+    let seed = xxh3_64(input_bytes);
+    let mut rng = StdRng::seed_from_u64(seed);
 
-    result = {
-        let mut new_result = String::new();
-        let mut last_end = 0;
-        for (start, part) in result.match_indices("aww") {
-            let result_bytes = result.as_bytes();
-            if start + part.len() >= result_bytes.len() { continue; }
-            match result_bytes[start + part.len()] {
-                b'w' => {},
-                b' ' => {},
-                _ => continue
-            };
+    let input_length = input.len();
+    let mut result = String::with_capacity(input_length);
 
-            new_result.push_str(unsafe { result.get_unchecked(last_end..start) });
-            new_result.push_str("uwu");
-            last_end = start + part.len();
+    result.push_str(EMOJIS[rng.gen_range(0..EMOJIS.len())]);
+    result.push(' ');
+
+    let mut i = 0;
+    while i < input_length {
+        if input_bytes[i] == b'r' || input_bytes[i] == b'l' {
+            result.push('w');
+            i += 1;
+            continue;
         }
-        new_result.push_str(unsafe { result.get_unchecked(last_end..result.len()) });
-        new_result
-    };
 
-    result = {
-        let mut new_result = String::new();
-        let mut last_end = 0;
-        for (start, part) in result.match_indices("awe") {
-            let result_bytes = result.as_bytes();
-            if start + part.len() >= result_bytes.len() { continue; }
-            if start + part.len() + 4 >= result_bytes.len() { continue; }
-            if &result_bytes[start + part.len()..start + part.len() + 4] != "some".as_bytes() {
+        for (from, to) in SIMPLE_REPLACEMENTS {
+            if input.match_single(i, from) {
+                result.push_str(to);
+                i += from.len();
                 continue;
             }
-            new_result.push_str(unsafe { result.get_unchecked(last_end..start) });
-            new_result.push_str("uwu");
-            last_end = start + part.len();
         }
-        new_result.push_str(unsafe { result.get_unchecked(last_end..result.len()) });
-        new_result
-    };
 
-    result = {
-        let mut new_result = String::new();
-        let mut last_end = 0;
-        for (start, part) in result.match_indices("n") {
-            let result_bytes = result.as_bytes();
-            if start + part.len() >= result_bytes.len() { continue; }
-            match &result_bytes[start + part.len()] {
-                b'a' => {},
-                b'e' => {},
-                b'i' => {},
-                b'o' => {},
-                b'u' => {},
-                _ => continue
+        if input.match_single(i, "aww") && (i + 3 >= input_length || input_bytes[i + 3] == b'w' || input_bytes[i + 3] == b' ') {
+            result.push_str("uwu");
+            i += 3;
+            continue;
+        }
+
+        if input.match_single(i, "awesome") {
+            result.push_str("uwu");
+            i += 3;
+            continue;
+        }
+
+        const NYA_NEEDLES: [&str; 5] = ["na", "ne", "ni", "no", "nu"];
+        if input.match_any(i, &NYA_NEEDLES) {
+            result.push_str("ny");
+            i += 1;
+            continue;
+        }
+
+        if input_bytes[i] == b'.' && (i != 0 && input_bytes[i - 1] != b'.') {
+            let mut j = i;
+            let meets_condition = loop {
+                if j >= input_length {
+                    break true;
+                }
+                if input_bytes[j] != b'.' {
+                    if input_bytes[j] == b' ' {
+                        break false;
+                    }
+                    break true;
+                }
+                j += 1;
             };
 
-            new_result.push_str(unsafe { result.get_unchecked(last_end..start) });
-            new_result.push_str("ny");
-            last_end = start + part.len();
-        }
-        new_result.push_str(unsafe { result.get_unchecked(last_end..result.len()) });
-        new_result
-    };
-
-    result = {
-        let mut new_result = String::new();
-        let mut last_end = 0;
-        for (start, part) in result.match_indices(".") {
-            let result_bytes = result.as_bytes();
-            if start == 0 { continue };
-            if result_bytes[start - 1] == b'.' { continue };
-            if start + part.len() < result_bytes.len() {
-                if result_bytes[start + part.len()] != b' ' { continue };
+            if meets_condition {
+                let s = if rng.gen_bool(0.5) { EMOJIS[rng.gen_range(0..EMOJIS.len())] } else { PHRASES[rng.gen_range(0..PHRASES.len())] };
+                result.push(' ');
+                result.push_str(s);
+                result.push('.');
+                i += 1;
+                continue;
             }
-
-            new_result.push_str(unsafe { result.get_unchecked(last_end..start) });
-            new_result.push_str("uwu");
-            last_end = start + part.len();
         }
-        new_result.push_str(unsafe { result.get_unchecked(last_end..result.len()) });
-        new_result
-    };
 
-    return result;
+        if input_bytes[i] == b',' && (i + 1 >= input_length || input_bytes[i + 1] == b' ') {
+            let s = PHRASES[rng.gen_range(0..PHRASES.len())];
+            result.push(' ');
+            result.push_str(s);
+            result.push(',');
+            i += 1;
+            continue;
+        }
+
+        if input_bytes[i] == b'!' && (i + 1 >= input_length || input_bytes[i + 1] == b' ' || input_bytes[i + 1] == b'!') {
+            result.push_str("!!");
+            i += 1;
+            continue;
+        }
+
+        if input_bytes[i] == b'?' && (i + 1 >= input_length || input_bytes[i + 1] == b' ' || input_bytes[i + 1] == b'?') {
+            result.push_str(if rng.gen_bool(0.5) { "?!" } else { "!?" });
+            i += 1;
+            continue;
+        }
+
+        if input.match_single(i, "wo") && (i == 0 || input_bytes[i - 0] == b'o') && (i + 1 >= input_length || input_bytes[i + 1] == b'w') {
+            result.push_str("owo");
+            i += 2;
+            continue;
+        }
+
+        result.push(char::from(input_bytes[i]));
+        i += 1;
+    }
+
+    result.push(' ');
+    result.push_str(PHRASES[rng.gen_range(0..PHRASES.len())]);
+
+    result
 }
 
 fn main() {
-    println!("{}", uwuify("love nice what you the aww awwwww awesome na ne ni no nu"));
+    println!("{}", uwuify("In the enchanted forest, a sentient cat named Mia explored the glistening glades with curiosity in her big, innocent eyes. She playfully chased after sunbeams that danced through the canopy, and her soft, silk-like fur ruffled gently in the breeze. Mia's sweet and gentle nature were captivating, endearing her to all who encountered her."));
+}
+
+pub trait Match {
+    fn match_single(&self, index: usize, needle: &str) -> bool;
+
+    fn match_any(&self, index: usize, needles: &[&str]) -> bool;
+}
+impl Match for &str {
+    fn match_single(&self, index: usize, needle: &str) -> bool {
+        if index + needle.len() > self.len() {
+            return false;
+        }
+
+        self[index..].starts_with(needle)
+    }
+
+    fn match_any(&self, index: usize, needles: &[&str]) -> bool {
+        for &needle in needles {
+            if index + needle.len() > self.len() { continue; }
+            if self[index..].starts_with(needle) {
+                return true;
+            }
+        }
+
+        false
+    }
 }
